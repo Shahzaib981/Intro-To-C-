7c72dd91c887d52cb16e52e185e39c1c5f83e2c3
Shahzaib981 <62476855+Shahzaib981@users.noreply.github.com>
2020-03-21 22:27:58 +0500

    Exception Handling Programs

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+
+
+//practical example of inheritance
+
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader(@"C:\VS Programs\log.txt");
+                Console.WriteLine(reader.ReadToEnd());
+                reader.Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Please check if the file {0} ", ex.FileName);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+                Console.WriteLine("Block");
+            }
+        }
+    }
+}
