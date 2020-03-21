2a460d94a2b590415a7f2e12542440e34e65257c
Shahzaib981 <62476855+Shahzaib981@users.noreply.github.com>
2020-03-21 22:13:24 +0500

    Abstract Class Programs
    Abstract Class

---
+++ Abstraction/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+                                                //Abstract class Program
+
+using System;
+
+namespace Abstraction
+{
+
+    abstract class animal
+    {
+        public abstract void animalsound();
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    class dog : animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("The Dog barks");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dog a1 = new dog();
+            a1.animalsound();
+            a1.sleep();
+        }
+    }
+}
