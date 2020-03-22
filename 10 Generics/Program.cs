d436f9ec4e19d0888a36ae54455d4faaeb1cff6b
Shahzaib981 <62476855+Shahzaib981@users.noreply.github.com>
2020-03-22 19:04:53 +0500

    Program related to Generics

---
+++ 10 Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            bool Equal = Calculator.AreEqual<string>("AB", "AB");
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+        
+    }
+
+    public class Calculator
+    {
+        public static bool AreEqual<T>(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+
+    }
+}
